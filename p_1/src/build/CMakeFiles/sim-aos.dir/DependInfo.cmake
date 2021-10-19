f264d6733928ead122b21426a323bcfb42614cab
Gonzalo Juarez <gonza4266118@gmail.com>
2021-10-19 15:59:38 +0200

    test script

---
+++ p_1/src/build/CMakeFiles/sim-aos.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/shared/gonzalo/Facultad/uc3m/arcos/lab/p_1/src/obj.cpp" "CMakeFiles/sim-aos.dir/obj.cpp.o" "gcc" "CMakeFiles/sim-aos.dir/obj.cpp.o.d"
+  "/mnt/shared/gonzalo/Facultad/uc3m/arcos/lab/p_1/src/sim.cpp" "CMakeFiles/sim-aos.dir/sim.cpp.o" "gcc" "CMakeFiles/sim-aos.dir/sim.cpp.o.d"
+  "/mnt/shared/gonzalo/Facultad/uc3m/arcos/lab/p_1/src/util.cpp" "CMakeFiles/sim-aos.dir/util.cpp.o" "gcc" "CMakeFiles/sim-aos.dir/util.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ p_1/src/build/CMakeFiles/sim-aos.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sim-aos.dir/obj.cpp.o"
+  "CMakeFiles/sim-aos.dir/obj.cpp.o.d"
+  "CMakeFiles/sim-aos.dir/sim.cpp.o"
+  "CMakeFiles/sim-aos.dir/sim.cpp.o.d"
+  "CMakeFiles/sim-aos.dir/util.cpp.o"
+  "CMakeFiles/sim-aos.dir/util.cpp.o.d"
+  "sim-aos"
+  "sim-aos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sim-aos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
