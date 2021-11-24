84593df25d507de37295448590af9c93dc2ade8b
Gonzalo Juarez <gonza4266118@gmail.com>
2021-11-24 13:42:25 +0100

    borrador 1 memoria completa

--- p_2/src/tests/hipotesis.py
+++ p_2/src/tests/hipotesis.py
@@ -23,15 +23,13 @@ def get_data(filenames):
 def kruskal_wallis(filenames, file_data):
     tests = [(4000,250), (4000,500), (8000,250), (8000,500)]
     res = {}
+
     for t in tests:
-        res[t] = st.kruskal(
-                file_data[filenames[0]][t[0]][t[1]],
-                file_data[filenames[1]][t[0]][t[1]],
-                file_data[filenames[2]][t[0]][t[1]],
-                file_data[filenames[3]][t[0]][t[1]],
-                file_data[filenames[4]][t[0]][t[1]],
-                file_data[filenames[5]][t[0]][t[1]],
-                )
+        kruskal_args = []
+        for f in filenames:
+            kruskal_args.append(file_data[f][t[0]][t[1]])
+        res[t] = st.kruskal(*kruskal_args)
+
     print("Resultados Kruskal-Wallis H-Test:")
     for k in res:
         print("\nConfiguración: num_iterations: %d, num_objects: %d, p_valor: %.12f" % (k[0], k[1], res[k].pvalue))
@@ -40,7 +38,7 @@ def kruskal_wallis(filenames, file_data):
         else:
             print("\tSí hay diferencia significativa en las medias de las muestras")
 
-def t_test(sample_a, sample_b):
+def t_test(sample_a, sample_b, filename_a, filename_b):
     tests = [(4000,250), (4000,500), (8000,250), (8000,500)]
     res = {} #st.ttest_ind(a=sample_a,b=sample_b,equal_var=True)
 
@@ -56,16 +54,15 @@ def t_test(sample_a, sample_b):
             print("\tSí hay diferencia significativa en las medias de las muestras")
             mean_a = numpy.mean(sample_a[k[0]][k[1]])
             mean_b = numpy.mean(sample_b[k[0]][k[1]])
-            print("\t\tmedia a: %f, media b: %f" % (mean_a, mean_b))
+            print("\t\tmedia %s: %f, media %s: %f" % (filename_a, mean_a, filename_b, mean_b))
             if mean_a > mean_b:
                 print("\t\t\tSpeedup = %f" % (mean_a / mean_b))
             else:
                 print("\t\t\tSpeedup = %f" % (mean_b / mean_a))
 
 if __name__ == "__main__":
-    filenames = ["p_1_samples", "1core_samples", "2core_samples", "4core_samples",
-            "8core_samples", "16core_samples"]
+    filenames = sys.argv[1:]
     file_data = get_data(filenames)
     kruskal_wallis(filenames, file_data)
     print("\n")
-    t_test(file_data["p_1_samples"], file_data["8core_samples"])
+    t_test(file_data[filenames[0]], file_data[filenames[1]], filenames[0], filenames[1])
