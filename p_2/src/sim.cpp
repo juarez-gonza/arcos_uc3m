4deb8c12ff1b9468f40dbcf50565552ead2a8bbb
Gonzalo Juarez <gonza4266118@gmail.com>
2021-11-25 11:27:37 +0100

    no_if_aos

--- p_2/src/sim.cpp
+++ p_2/src/sim.cpp
@@ -41,8 +41,6 @@ static inline void calc_fgv(struct obj &o_i, struct obj &o_j)
 
 	o_i.fz = o_i.fz + fz;
 	o_j.fz = o_j.fz - fz;
-
-	//printf("o.fx: %f, o.fy: %f, o.fz: %f\n", o_i.fx, o_i.fy, o_i.fz);
 }
 
 static inline void calc_vel(struct obj &o, double time_step)
@@ -52,7 +50,6 @@ static inline void calc_vel(struct obj &o, double time_step)
 	o.vx = o.vx + accel_no_recalc * o.fx;
 	o.vy = o.vy + accel_no_recalc * o.fy;
 	o.vz = o.vz + accel_no_recalc * o.fz;
-	//printf("o.vx: %f, o.vy: %f, o.vz: %f\n", o.vx, o.vy, o.vz);
 }
 
 static inline void adjust_limits(double size_enclosure,
@@ -78,7 +75,6 @@ static inline void calc_pos(struct obj &o, double size_enclosure, double time_st
 
 	o.z = o.z + o.vz * time_step;
 	adjust_limits(size_enclosure, o.z, o.vz);
-	//printf("o.x: %f, o.y: %f, o.z: %f\n", o.x, o.y, o.z);
 }
 
 static inline void mark(struct obj &o)
@@ -167,13 +163,7 @@ int main(int argc, char *argv[])
 	if (write_config("init_config.txt", arg_list.size_enclosure, arg_list.time_step, o_list))
 		log_n_exit("Error while trying to write to init_config.txt\n", 1);
 
-	double tic = omp_get_wtime();
-
 	simulate(o_list, arg_list.num_iterations, arg_list.size_enclosure, arg_list.time_step);
-
-	double toc = omp_get_wtime();
-
-	printf("tiempo de ejecucion: %f\n", toc-tic);
 
 	if (write_config("final_config.txt", arg_list.size_enclosure, arg_list.time_step, o_list))
 		log_n_exit("Error while trying to write to final_config.txt\n", 1);
