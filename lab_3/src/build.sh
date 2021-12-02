27c643ffe99486f82c486ceff528207345ccedc4
Gonzalo Juarez <gonza4266118@gmail.com>
2021-12-02 01:57:33 +0100

    tests

--- lab_3/src/build.sh
+++ lab_3/src/build.sh
@@ -5,8 +5,10 @@ pushd build/
 rm -rf *
 
 if [ "$1" = "d" ]; then
+	echo "debug"
 	cmake ../ -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_CXX_FLAGS="-fsanitize=thread"
 else
+	echo "release"
 	cmake ../ -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_CXX_FLAGS="-fsanitize=thread"
 fi
 
